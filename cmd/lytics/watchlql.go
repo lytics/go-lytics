b98ce9ede248575e251cac5255d9f22598219428
Aaron Raddon <aaron@lytics.io>
2016-11-03 17:17:41 -0700

    Watch for lql/json files to evaluate

--- cmd/lytics/watchlql.go
+++ cmd/lytics/watchlql.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
+	"net/url"
 	"strings"
 
 	"github.com/fsnotify/fsnotify"
+	lytics "github.com/lytics/go-lytics"
 )
 
 /*
@@ -21,62 +27,157 @@ Watch a folder for changes and output evaluated entity
 
 func (c *Cli) watch() (interface{}, error) {
 
-	l := newLql()
+	l := newLql(c)
+	l.start()
 
-	done := make(chan bool)
-	<-done
-
-	return nil, err
+	return nil, nil
 }
 
 type datafile struct {
-	name     string
-	datatype string
-}
-type lql struct {
-	files map[string]string
-	data  map[string]*datafile
+	name string
+	lql  string
+	data []url.Values
 }
 
-func newLql() *lql {
-	l := &lql{}
-	l.start()
-	return l
+func (d *datafile) loadJson(of string) {
+	by, err := ioutil.ReadFile("./" + of)
+	exitIfErr(err, fmt.Sprintf("Could not read json file %v", of))
+	l := make([]map[string]interface{}, 0)
+	err = json.Unmarshal(MakeJsonList(by), &l)
+	exitIfErr(err, "Invalid json file")
+
+	qsargs := make([]url.Values, 0, len(l))
+	for _, row := range l {
+		qs, err := lytics.FlattenJsonMap(row)
+		if err == nil {
+			qsargs = append(qsargs, qs)
+		} else {
+			log.Printf("Could not convert row to qs? %v   %v\n", row, err)
+		}
+	}
+	d.data = qsargs
+}
+
+type lql struct {
+	files map[string]*datafile
+	w     *fsnotify.Watcher
+	c     *Cli
+}
+
+func newLql(c *Cli) *lql {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		panic(err.Error())
+	}
+	return &lql{
+		c:     c,
+		w:     watcher,
+		files: make(map[string]*datafile),
+	}
+}
+
+func (l *lql) start() {
+	defer l.w.Close()
+	done := make(chan bool)
+	l.loadFiles()
+	l.watch()
+	<-done
+}
+func (l *lql) print(d *datafile) {
+	if len(d.data) == 0 {
+		log.Printf("No data found for %v \n\n", d.name)
+		return
+	}
+	if len(d.lql) == 0 {
+		log.Printf("No lql found for %v \n\n", d.name)
+		return
+	}
+	for _, qs := range d.data {
+		ent, err := l.c.Client.GetQueryTest(qs, d.lql)
+		if err != nil {
+			log.Printf("Could not evaluate query/entity: %v \n\n", err)
+			return
+		}
+		out, err := json.MarshalIndent(ent, "", "  ")
+		if err == nil {
+			fmt.Printf("\n%v\n\n", string(out))
+		}
+	}
+
+}
+
+func (l *lql) handleFile(of string, showOutput bool) {
+	if strings.Index(of, ".") < 1 {
+		return
+	}
+	f := strings.ToLower(of)
+	name := strings.Split(f, ".")[0]
+	df, exists := l.files[name]
+	if !exists {
+		df = &datafile{name: name}
+		l.files[name] = df
+	}
+	switch {
+	case strings.HasSuffix(f, ".lql"):
+		log.Println("handle lql file ", f)
+		by, err := ioutil.ReadFile("./" + of)
+		exitIfErr(err, fmt.Sprintf("Could not read file %v", of))
+		df.lql = string(by)
+	case strings.HasSuffix(f, ".csv"):
+		log.Println("handle csv file ", f)
+	case strings.HasSuffix(f, ".json"):
+		log.Println("handle json file ", f)
+		df.loadJson(of)
+	default:
+		return
+	}
+	if showOutput {
+		l.print(df)
+	}
+}
+
+func (l *lql) loadFiles() {
+	files, _ := ioutil.ReadDir("./")
+	for _, f := range files {
+		l.handleFile(f.Name(), false)
+	}
 }
 
 func (l *lql) watch() {
 
-	watcher, err := fsnotify.NewWatcher()
-	if err != nil {
-		return nil, err
-	}
-	defer watcher.Close()
-
 	go func() {
 		for {
 			select {
-			case event := <-watcher.Events:
+			case event := <-l.w.Events:
 				//log.Println("event:", event)
 				if event.Op&fsnotify.Write == fsnotify.Write || event.Op&fsnotify.Create == fsnotify.Create {
 					fn := strings.ToLower(event.Name)
-					log.Println("modified file:", fn)
-					switch {
-					case strings.HasSuffix(fn, ".lql"):
-						// edited lql
-					}
+					fn = strings.Replace(fn, "./", "", 1)
+					//log.Println("modified file:", fn)
+					l.handleFile(fn, true)
 				}
-			case err := <-watcher.Errors:
-				log.Println("watch error:", err)
+			case err, ok := <-l.w.Errors:
+				if !ok {
+					log.Fatal("What, no errors channel")
+				} else {
+					log.Println("watch error:", err)
+				}
+
 			}
 		}
 	}()
 
-	err = watcher.Add("./")
-	if err != nil {
+	if err := l.w.Add("./"); err != nil {
 		log.Fatal(err)
 	}
 }
 
-func (l *lql) print() {
-
+// Convert a slice of bytes into an array by ensuring it is wrapped
+//  with []
+func MakeJsonList(b []byte) []byte {
+	if !bytes.HasPrefix(b, []byte{'['}) {
+		b = append([]byte{'['}, b...)
+		b = append(b, ']')
+	}
+	return b
 }
---
+++ query.go
@@ -0,0 +1,67 @@
+package lytics
+
+import (
+	"net/url"
+	"time"
+)
+
+const (
+	queryEndpoint     = "query/:id"
+	queryListEndpoint = "query"
+	queryTestEndpoint = "query/_test"
+)
+
+type Query struct {
+	Id      string                `json:"id"`
+	Created time.Time             `json:"created"`
+	Updated time.Time             `json:"updated"`
+	Alias   string                `json:"alias"`
+	Table   string                `json:"table"`
+	Text    string                `json:"text"`
+	Fields  map[string]QueryField `json:"fields"`
+}
+type QueryField struct {
+	As         string   `json:"as"`
+	IsBy       bool     `json:"is_by"`
+	Type       string   `json:"type"`
+	ShortDesc  string   `json:"shortdesc"`
+	LongDesc   string   `json:"longdesc"`
+	Identities []string `json:"identities"`
+	Froms      []string `json:"froms"`
+}
+
+// GetQueries returns a list of all queries associated with this account
+// https://www.getlytics.com/developers/rest-api#query
+func (l *Client) GetQueries() ([]Query, error) {
+	res := ApiResp{}
+	data := []Query{}
+
+	// make the request
+	err := l.Get(queryListEndpoint, nil, nil, &res, &data)
+	if err != nil {
+		return data, err
+	}
+
+	return data, nil
+}
+
+// GetQueryTest returns the evaluated entity from given query
+// https://www.getlytics.com/developers/rest-api#segment-sizes
+func (l *Client) GetQueryTest(qs url.Values, query string) (Entity, error) {
+	res := ApiResp{}
+	data := Entity{}
+
+	// make the request
+	err := l.Post(queryTestEndpoint, qs, query, &res, &data)
+
+	if err != nil {
+		return data, err
+	}
+
+	return data, nil
+}
+
+// Other Available Endpoints
+// * POST    create account
+// * PUT     upate account
+// * DELETE  remove account
